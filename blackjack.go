7c4f3beb29daba2ee1f8d8ca962fc9186f4b5d38
garrettrogers002 <garrett.rogers002@gmail.com>
2025-04-04 22:34:01 -0400

    change cards to a dictionary format

--- blackjack.go
+++ blackjack.go
@@ -6,14 +6,25 @@ import "fmt"
 func createDeck() {
 	var suits = [4]string{"Clubs", "Spades", "Hearts", "Diamonds"}
 	var values = [13]string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
-	cards := []string{}
+	cards := []map[string]interface{}{}
+
+
+
 	// fmt.Println("Len of values: ", len(values))
 	// fmt.Printf("%T\n", suits)
 	// fmt.Println(len(suits))
 	for i := 0; i < len(suits); i++ {
 		for j := 0; j < len(values); j++ {
-			cardName := suits[i] + " of " + values[j]
-			cards = append(cards, cardName)
+			// cardName := suits[i] + " of " + values[j]
+			card := map[string]interface{}{
+				"suit": suits[i],
+				"value": values[j],
+				"number": j + 1,
+
+				"gameValue": "getCardValue(j + 1)",
+			}
+				
+			cards = append(cards, card)
 		}
 	}
 	fmt.Println(len(cards))
